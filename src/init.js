9bed62ecd79697c4d3ed3fafaef4dab0baebdc16
joyboy10 <csbt1033@gmail.com>
2022-02-19 23:12:58 +0900

    port

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/user";
 import "./models/comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const openServer = () => {
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 };
