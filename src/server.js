f3df26f4d927fe670d94c6905d32648369468c71
joyboy10 <csbt1033@gmail.com>
2022-02-20 12:46:26 +0900

    plz working

--- src/server.js
+++ src/server.js
@@ -20,11 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(flash());
-// app.use((req, res, next) => {
-//   res.header("Cross-Origin-Embedder-Policy", "require-corp");
-//   res.header("Cross-Origin-Opener-Policy", "same-origin");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
